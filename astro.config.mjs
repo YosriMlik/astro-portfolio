6cbc6fa8c2225dc0005c56611941b3cc3b3d1697
YosriMlik <ymlik24@gmail.com>
2025-10-23 10:21:11 +0100

    change output config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // Enable static site generation
-  output: 'static',
+  // Enable static site generation
+  // output: 'static',
   
   // // Build configuration
   // build: {
